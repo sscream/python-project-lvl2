a2704e2f0f2d6cd81df771ada05b61cedcd02921
Sergey Sakirko <ssakirko@mail.ru>
2020-10-08 20:54:52 +0300

    step 2

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,5 +4,6 @@ def main():
     parser = argparse.ArgumentParser(description='Generate diff.')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
+    parser.add_argument('-f', '--format', type=str, help='set format of output')
 
     args = parser.parse_args()
