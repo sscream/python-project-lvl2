4576b9e5114087d549167ae5ebf5ff4206e2c40b
Sergey Sakirko <ssakirko@mail.ru>
2020-10-08 20:46:10 +0300

    step 1

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,8 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate diff.')
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+
+    args = parser.parse_args()
