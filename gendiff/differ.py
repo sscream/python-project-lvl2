460ea01189faffe32a36274182d2e17c930bec74
Sergey Sakirko <ssakirko@mail.ru>
2020-11-07 12:08:30 +0300

    parser refactoring

--- gendiff/differ.py
+++ gendiff/differ.py
@@ -1,3 +1,6 @@
+import os
+from typing import Union
+
 from gendiff import (
     get_file_data,
     tree,
@@ -5,12 +8,26 @@ from gendiff import (
 )
 
 
+def read_file(file_path: str):
+    return open(file_path, 'r')
+
+
+def get_file_extension(filename: Union[str, os.PathLike]) -> str:
+    return os.path.splitext(filename)[1][1:]
+
+
 def generate_diff(
     file_path1: str, file_path2: str, formatter_name: str = formatters.STYLISH
 ) -> str:
 
-    data1 = get_file_data(file_path1)
-    data2 = get_file_data(file_path2)
+    file1 = read_file(file_path1)
+    file2 = read_file(file_path2)
+
+    data1 = get_file_data(file1, get_file_extension(file_path1))
+    data2 = get_file_data(file2, get_file_extension(file_path2))
+
+    file1.close()
+    file2.close()
 
     diff = tree.build_diff(data1, data2)
 
--- gendiff/parsers.py
+++ gendiff/parsers.py
@@ -1,22 +1,13 @@
-import os
 import json
-from typing import Union
+from io import TextIOWrapper
 
 import yaml
 
 
-def get_file_data(file_path: Union[str, os.PathLike]) -> dict:
-    extension = get_file_extension(os.path.basename(file_path))
-
-    with open(file_path, 'r') as file:
-
-        if extension == 'json':
-            return json.load(file)
-        elif extension in {'yml', 'yaml'}:
-            return yaml.safe_load(file)
-        else:
-            raise ValueError('Unsupported file format.')
-
-
-def get_file_extension(filename: Union[str, os.PathLike]) -> str:
-    return os.path.splitext(filename)[1][1:]
+def get_file_data(file: TextIOWrapper, extension: str) -> dict:
+    if extension == 'json':
+        return json.load(file)
+    elif extension in {'yml', 'yaml'}:
+        return yaml.safe_load(file)
+    else:
+        raise ValueError('Unsupported extension.')
